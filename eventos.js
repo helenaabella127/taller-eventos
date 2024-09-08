7f55f6013d4113f6f03a541e17944f50fd878fb4
helenaabella127 <sofiAbella10@gmail.com>
2024-09-08 20:02:30 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -2,3 +2,9 @@ document.getElementById("mi-div").addEventListener("click", function() {
     alert("Hola! Soy el div");
 });
 
+// Manejador del botón para evitar la propagación del evento
+const boton = document.querySelector("button");
+boton.addEventListener("click", function(event) {
+    event.stopPropagation(); // Evita que el evento se propague al div
+    alert("Hola! Soy el botón");
+});
